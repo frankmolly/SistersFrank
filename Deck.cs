450565bc730ac8db5dbf5f7acef7a2883634493a
Hannah Frank <frank_h@students.lynchburg.edu>
2014-03-19 21:36:50 -0400

    3/19/2014 H&M Changes
    Changed Data Members.
    Added to the constructor.

--- Deck.cs
+++ Deck.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,30 @@ namespace Solitaire350
 {
     public class Deck
     {
+        public const int CAPACITY = 52;
         public Deck()
         {
-            Card[] deckOfCards = new Card[0];
             size = 0;
-            capacity = 52;
+            Card[] deckOfCards = new Card[CAPACITY];
+
+            for (int i = 0; i < Card.MAX_SUIT; i++)
+            {
+                for (int j = 0; j < Card.MAX_RANK; j++)
+                {
+                    deckOfCards[size] = new Card(Card.RankType., Card.SuitType.i);
+                }
+            }
+            
+        }
+
+        public int size { get; private set; }
+        
+        public void addCard(Card cardToAdd)
+        {
+            if (size != CAPACITY)
+            {
+                deckOfCards[size] = cardToAdd;
+            }
         }
 
         /*public bool isEqual(Card rhs)
@@ -21,8 +40,8 @@ namespace Solitaire350
         }*/
 
         public Card deckOfCards { get; private set; }
-        public int size { get; private set; }
-        public int capacity { get; private set; }
+       
+        
 
     }
 }
