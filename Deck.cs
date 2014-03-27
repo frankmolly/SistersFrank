957b9e260522b7107a036c504ce8bfce1e14cdb0
Hannah Frank <frank_h@students.lynchburg.edu>
2014-03-26 21:44:59 -0400

    Update Deck.cs

--- Deck.cs
+++ Deck.cs
@@ -3,41 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Solitaire350
 {
-    public class Deck
+  
+    [TestFixture]
+    public class TestCard
     {
-        public const int CAPACITY = 52;
-        public Deck()
+        [Test]
+        public void createCard() //tests that a card can be created of a designated suit and rank
         {
-            size = 0;
-            Card[] deckOfCards = new Card[CAPACITY];
-
-            for (int i = 0; i < Card.MAX_SUIT; i++)
-            {
-                for (int j = 0; j < Card.MAX_RANK; j++)
-                {
-                    deckOfCards[size] = new Card((Card.RankType) j, (Card.SuitType) i);
-                    ++size;
-                }
-            }
-
+            //const int MY_CARD_RANK = 3;
+            Card myCard = new Card(Card.RankType.ACE, Card.SuitType.CLUBS); //created a card of rank ace and suit clubs
+            Assert.AreEqual(myCard.Suit, Card.SuitType.CLUBS); //assert that the card's suit is equal to clubs
+            Assert.AreEqual(myCard.Rank, Card.RankType.ACE); //assert that the card's rank is equal to ace
         }
 
-        //public void printDeck()
-        //{
-        //    for(int i=0;i<size;i++)
-        //    {
-        //        Console.WriteLine("{0}", deckOfCards[i]);
-        //    }
-        //}
 
-        public int size { get; private set; }
+        [Test]
+        public void equalCards() //tests that two cards of the same type and rank are the same
+        {
+            Card myCard = new Card(Card.RankType.THREE, Card.SuitType.CLUBS);
+            Card yourCard = new Card(Card.RankType.THREE, Card.SuitType.CLUBS);
+            Assert.IsTrue(myCard.isEqual(yourCard));
+        }
 
-        public Card deckOfCards { get; private set; }
-       
-        
-
+        [Test]
+        public void printCard() //tests our print card function
+        {
+            Card myCard = new Card(Card.RankType.ACE, Card.SuitType.SPADES);
+            myCard.printCard();
+        }
     }
+
+    [TestFixture]
+    public class TestDeck
+    {
+        const int DECK_SIZE = 52;
+
+         [Test]
+        public void createDeck() //tests that a deck can be created with ability
+                                //to hold 52 cards
+        {
+            Card[] newDeck = new Card[DECK_SIZE]; 
+            Deck myDeck = new Deck();
+            Assert.AreEqual(myDeck.size, DECK_SIZE);
+            Assert.Contains
+        }
+
+
+         [Test]
+         public void swap()
+         {
+             Deck myDeck = new Deck();
+             Deck swappedCards = new Deck();
+             swappedCards.swap(0,1);
+             Assert.AreNotEqual(myDeck[0], swappedCards[0]);
+             Assert.AreNotEqual(myDeck[1], swappedCards[1]);
+             Assert.AreEqual(myDeck[0], swappedCards[1]);
+             Assert.AreEqual(myDeck[1], swappedCards[0]);
+         }
+
+         [Test]
+         public void shuffle() //tests to see if the cards are being randomly shuffled
+         {
+             Deck unshuffledDeck = new Deck();
+             Deck shuffledDeck = new Deck();
+             shuffledDeck.shuffle();
+             Assert.AreNotEqual(unshuffledDeck, shuffledDeck); //a shuffled and an unshuffled deck should not be equal
+         }
+
+        
+    }
+
+    
 }
